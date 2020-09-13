4d0fc63604e0f414b5137333393672cdc19a63a7
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-09-13 11:05:30 +0700

    pdf fix

--- pdf_generator/views.py
+++ pdf_generator/views.py
@@ -14,10 +14,11 @@ def rat_pedigree_view(request, data):
     pdfmetrics.registerFont(TTFont('Verdana', 'Verdana.ttf'))
 
     rat = Rat.objects.get(id=data)
+    print('RAT:', rat.variety)
 
     response = HttpResponse(content_type='application/pdf')
     # файл для скачивания
-    # response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+    # response['Content-Disposition'] = 'attachment; filename="rat_pedigree.pdf"'
     # файл для чтения
     response['Content-Disposition'] = 'filename="rat_pedigree.pdf"'
 
@@ -28,8 +29,10 @@ def rat_pedigree_view(request, data):
     pdf.drawString(10, 820, f'Имя: {rat.name} {rat.prefix}')
     pdf.drawString(10, 810, f'Тип: {rat.variety}')
     pdf.drawString(10, 800, f'Дата рождения: {rat.date_of_birth}')
-    pdf.drawString(10, 780, f'Отец: {rat.father.name} {rat.father.prefix} ({rat.father.variety})')
-    pdf.drawString(10, 770, f'Мать: {rat.mother.name} {rat.mother.prefix} ({rat.mother.variety})')
+    if rat.father:
+        pdf.drawString(10, 780, f'Отец: {rat.father.name} {rat.father.prefix} ({rat.father.variety})')
+    if rat.mother:
+        pdf.drawString(10, 770, f'Мать: {rat.mother.name} {rat.mother.prefix} ({rat.mother.variety})')
     pdf.showPage()
     pdf.save()
 
--- website/tests/test_model.py
+++ website/tests/test_model.py
@@ -7,11 +7,14 @@ class ImageModelTest(TestCase):
     """Тестирование модели Image"""
 
     def test_can_saving_and_retrieving_images(self):
-        Image.objects.create(name='first')
+        entry = Entry.objects.create()
+        Image.objects.create(main_page=True, name='first', entry=entry)
         Image.objects.create(name='second')
         images = Image.objects.all()
         self.assertEqual(images.count(), 2)
+        self.assertEqual(images[0].main_page, True)
         self.assertEqual(images[0].name, 'first')
+        self.assertEqual(images[0].entry, entry)
         self.assertEqual(images[1].name, 'second')
 
 
@@ -19,9 +22,12 @@ class EntryModelTest(TestCase):
     """Тестирование модели Entry"""
 
     def test_can_saving_and_retrieving_entries(self):
-        Entry.objects.create(text='first')
+        Entry.objects.create(public=True, topic='test_topic', title='test_title', text='first')
         Entry.objects.create(text='second')
         entries = Entry.objects.all()
         self.assertEqual(entries.count(), 2)
+        self.assertEqual(entries[0].public, True)
+        self.assertEqual(entries[0].topic, 'test_topic')
+        self.assertEqual(entries[0].title, 'test_title')
         self.assertEqual(entries[0].text, 'first')
         self.assertEqual(entries[1].text, 'second')
